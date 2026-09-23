97c167f1f4709ae70a0143a89f48ba91f7e73ad4
Kunal Shivam Scaler <kunal.shivam@scalerailabs.com>
2026-09-23 15:01:33 +0530

    basic http routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,30 @@ const express = require('express');
 
 const app = express();
 
-app.use('/test',function(req,res){
-    res.send("this is test route");
+//for get http request 
+app.get("/user", ((req,res)=>{
+    res.send({
+        "firstName":"kunal",
+        "lastName":"shivam"
+    })
+}))
+
+//for post http request
+app.post("/user", (req,res)=>{
+    res.send("the data is saved in db")
 })
 
-app.use('/hello', ((req,res)=>{
-    res.send("hello from this route ")
-}))
+//for put http request
+app.put("/user", (req,res)=>{
+    res.end("the changes has been implemented")
+})
+
+//this will make all http request 
+app.use("/",function(req,res){
+    res.send("helo from main home route")
+})
+
+
 
 app.listen(7777,function(){
     console.log("server is succesfully started")
